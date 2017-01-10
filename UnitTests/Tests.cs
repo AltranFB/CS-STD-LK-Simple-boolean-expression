9d10ecadd24562983089c7513dd79bc4be4f3fdc
Grokwik <frederic@bodin.site>
2017-01-10 15:03:57 +0100

    Test categories

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -18,26 +18,31 @@ namespace Project
             Check.That(Solution.Test(5, 1)).IsTrue();
         }
 
+        [TestCategory("Reliability"), TestMethod]
         public void Test3()
         {
             Check.That(Solution.Test(2, 3)).IsFalse();
         }
 
+        [TestCategory("Reliability"), TestMethod]
         public void Test4()
         {
             Check.That(Solution.Test(3, 2)).IsFalse();
         }
 
+        [TestCategory("Language Knowledge"), TestMethod]
         public void Test5()
         {
             Check.That(Solution.Test(-3, 4)).IsTrue();
         }
 
+        [TestCategory("Language Knowledge"), TestMethod]
         public void Test6()
         {
             Check.That(Solution.Test(4, -3)).IsTrue();
         }
 
+        [TestCategory("Reliability"), TestMethod]
         public void Test7()
         {
             Check.That(Solution.Test(0, 0)).IsFalse();
