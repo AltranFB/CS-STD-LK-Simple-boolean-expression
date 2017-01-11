ef7f756baa30000429eea9b319be22195bf768f5
Grokwik <frederic@bodin.site>
2017-01-11 17:56:31 +0100

    Deleting unwanted dll + Test names

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -7,43 +7,43 @@ namespace Project
     public class Tests
     {
         [TestCategory("Language Knowledge"), TestMethod]
-        public void Test1()
+        public void LanguageKnowledge1()
         {
             Check.That(Solution.Test(1, 5)).IsTrue();
         }
 
         [TestCategory("Language Knowledge"), TestMethod]
-        public void Test2()
+        public void LanguageKnowledge2()
         {
             Check.That(Solution.Test(5, 1)).IsTrue();
         }
 
         [TestCategory("Reliability"), TestMethod]
-        public void Test3()
+        public void Reliability1()
         {
             Check.That(Solution.Test(2, 3)).IsFalse();
         }
 
         [TestCategory("Reliability"), TestMethod]
-        public void Test4()
+        public void Reliability2()
         {
             Check.That(Solution.Test(3, 2)).IsFalse();
         }
 
         [TestCategory("Language Knowledge"), TestMethod]
-        public void Test5()
+        public void LanguageKnowledge3()
         {
             Check.That(Solution.Test(-3, 4)).IsTrue();
         }
 
         [TestCategory("Language Knowledge"), TestMethod]
-        public void Test6()
+        public void LanguageKnowledge4()
         {
             Check.That(Solution.Test(4, -3)).IsTrue();
         }
 
         [TestCategory("Reliability"), TestMethod]
-        public void Test7()
+        public void Reliability3()
         {
             Check.That(Solution.Test(0, 0)).IsFalse();
         }
